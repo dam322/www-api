0d921b12151300ba06b27b32db5834f125d4b085
Ian2012 <crisgarta8@gmail.com>
2021-09-21 14:30:23 -0500

    Token Authentication

--- www_api/settings.py
+++ www_api/settings.py
@@ -35,6 +35,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
     ],
 }
 ROOT_URLCONF = 'www_api.urls'
