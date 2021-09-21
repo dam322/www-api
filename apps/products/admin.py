ea95ada1c5cf3b1ff6dc397c4daae698fae5bde0
Ian2012 <crisgarta8@gmail.com>
2021-09-20 21:49:23 -0500

    Modelo y Admin Product, Ingredient y Option creado y configurado.

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.products.models import Product, Ingredient, Option
+
+
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = (IngredientInline,)
+    list_filter = ('name', 'restaurant', 'is_customizable')
+    list_display = ('name', 'restaurant', 'is_customizable')
+
+
+class OptionInline(admin.TabularInline):
+    model = Option
+    extra = 1
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    inlines = (OptionInline,)
+    list_filter = ('name', 'product', 'is_available', 'is_obligatory')
+    list_display = ('name', 'product', 'is_available', 'is_obligatory')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from apps.users.models import Restaurant
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=32)
+    restaurant = models.ForeignKey(to=Restaurant, on_delete=models.CASCADE)
+    is_customizable = models.BooleanField()
+    price = models.IntegerField()
+    photo = models.ImageField(upload_to='products')
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=32)
+    order = models.IntegerField()
+    is_available = models.BooleanField()
+    is_obligatory = models.BooleanField()
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE, related_name='ingredients')
+
+    @property
+    def has_options(self):
+        return self.options.exists()
+
+
+class Option(models.Model):
+    name = models.CharField(max_length=32)
+    is_available = models.BooleanField()
+    ingredient = models.ForeignKey(to=Ingredient, on_delete=models.CASCADE, related_name='options')
