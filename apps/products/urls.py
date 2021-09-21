1368d23a6000317682d3c037c6650e2a85ffbb04
Ian2012 <crisgarta8@gmail.com>
2021-09-21 14:28:46 -0500

    Static URLs

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from apps.products.views import (
@@ -13,3 +15,4 @@ urlpatterns = [
     path('list-products/<int:pk>', ProductListByRestaurant.as_view())
     # TODO ProductIngredients
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
