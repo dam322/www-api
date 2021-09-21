6c9438c5e005036e920eb765af30a7a499857f1a
Ian2012 <crisgarta8@gmail.com>
2021-09-21 14:26:03 -0500

    Static folder

--- www_api/settings.py
+++ www_api/settings.py
@@ -85,7 +85,7 @@ USE_L10N = True
 USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
